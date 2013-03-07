a0fb295859a6ecdd74c744a6a785d08f9937351e
evismar.almeida <evismar.almeida@gmail.com>
2013-03-07 18:31:36 +0000

    Bd com generalização em 1  tabela

--- SrtaBiju/app/models/Cliente.java
+++ SrtaBiju/app/models/Cliente.java
@@ -13,17 +13,8 @@ import java.util.Set;
 
 
 @Entity
-@Table(name="Cliente")
-@AttributeOverrides({
-   @AttributeOverride(name="dataNascimento", column=@Column(name="dataNascimento")),
-   @AttributeOverride(name="email", column=@Column(name="email")),
-   @AttributeOverride(name="nome", column=@Column(name="nome")),
-   @AttributeOverride(name="senha", column=@Column(name="senha")),
-   @AttributeOverride(name="telefone1", column=@Column(name="telefone1")),
-   @AttributeOverride(name="telefone2", column=@Column(name="telefone2")),
-   @AttributeOverride(name="usuario", column=@Column(name="usuario")),
-   
-})
+@Table(name="PESSOA")
+@DiscriminatorValue("C")
 
 public class Cliente extends Pessoa{
    
@@ -41,12 +32,12 @@ public class Cliente extends Pessoa{
 
 
     @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "cliente_endereco", joinColumns = { @JoinColumn(name = "usuario_id"), @JoinColumn(name = "id") }, inverseJoinColumns = { @JoinColumn(name = "endereco_id") })
+    @JoinTable(name = "cliente_endereco", joinColumns = { @JoinColumn(name = "cliente_id")}, inverseJoinColumns = { @JoinColumn(name = "endereco_id") })
     public Set<Endereco> enderecos = new HashSet<Endereco>();
     
-    /*   
+  
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "cliente")
-    public List<Pedido> pedido;*/
+    public List<Pedido> pedido;
    
     public Cliente(String nome, String email, String telefone1, String telefone2, 
     		String facebook, String dataNascimento,String usuario,String senha) {
