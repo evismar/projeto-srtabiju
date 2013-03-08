b0b1d0128e78519c8bdff18016d96273ecc6c9cf
evismar.almeida@gmail.com <evismar.almeida@gmail.com@76877089-b45a-48bd-5a80-88a72267b327>
2013-03-08 00:35:00 +0000

    Mais validaçÕes de cliente

--- trunk/SrtaBiju/app/controllers/Clientes.java
+++ trunk/SrtaBiju/app/controllers/Clientes.java
@@ -19,17 +19,42 @@ public class Clientes extends Application{
     public static void salvarCadastro(@Valid String nome, String data, String telefone1, String telefone2,
     		String email, String facebook, String usuario, String senha, String confirmaSenha){
 
-
+    	//Verifica se as senhas estão combinado
         validation.required(senha);
-        validation.equals(confirmaSenha, senha).message("Suas senhas não estão combinado");   	
+        validation.required(confirmaSenha);
+        validation.equals(confirmaSenha, senha).message("Suas senhas não estão combinando");   	
     	
-    	//envia de volta ao formulario se houver erros..
+        //Verifica Mínimo e Maximo de caracteres das senhas
+        validation.minSize(senha, 4);
+        validation.maxSize(senha, 12);
+        
+        //Verifica se o email é válido
+        validation.email(email);
+        
+        
+    	//Verifica se há Logins iguais
+	    Pessoa pessoa = Pessoa.find("usuario", usuario).first();
+	    if(pessoa != null) {
+	    	System.out.println(pessoa.nome);
+	    	
+	        validation.addError("usuario", "Nome de usuário já cadastrado!"); 
+	    }
+	    
+	    if(pessoa != null) {
+	    	System.out.println(pessoa.email);
+	    	
+	        validation.addError("email", "Email já cadastrado!"); 
+	    }
+        
+        //envia de volta ao formulario se houver erros..
     	if(validation.hasErrors()) {
     	params.flash();
     	validation.keep();
     	cadCliente();
     	}    	
-    	        	
+    	
+    	//Cadastra
+    	
     	Cliente cliente = new Cliente(nome, data, telefone1, telefone2, email, facebook, usuario, senha);    	
     	Date dataCadastro = new Date();
         cliente.dataCadastro = dataCadastro;
