c0312935518d85e7f19f9eeb838b08b54b9c3506
evismar.almeida@gmail.com <evismar.almeida@gmail.com@76877089-b45a-48bd-5a80-88a72267b327>
2013-03-13 12:43:08 +0000

    Mudanças em sala

--- trunk/SrtaBiju/app/models/Administrador.java
+++ trunk/SrtaBiju/app/models/Administrador.java
@@ -21,20 +21,17 @@ public class Administrador extends Pessoa{
     
 
     public String tipo;
-    
 
-    public String facebook;
     
     
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "cliente")
     public List<Pedido> pedido;
    
-/*    public void Cliente(String nome, String email, String telefone1, String telefone2, String facebook, String dataNascimento,String usuario,String senha) {
+/*    public void Cliente(String nome, String email, String telefone1, String telefone2, String dataNascimento,String usuario,String senha) {
         this.nome = nome;
         this.email = email;
         this.telefone1 = telefone1;
         this.telefone2 = telefone2;
-        this.facebook = facebook;
         this.dataNascimento = dataNascimento;
         this.usuario = usuario;
         this.senha = senha;
